088a2de69ed027a0846d8cb5a89b9cca45e842ce
Zavaar Shah <35277113+ThatZiv@users.noreply.github.com>
2020-01-01 18:21:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ $(function(){
         var link = $('#link').val();
         var username = $('#username').val();
         var content = $('#content').val();
+        var avatar = $('#avatar').val();
         if (link==null || link=="",content==null || content=="")
         {
             alert("Please Fill Out All The Fields");
             return false;
         }
-        $.post(link, {"content": content, "username": username});
+        $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
 
     });
 });
